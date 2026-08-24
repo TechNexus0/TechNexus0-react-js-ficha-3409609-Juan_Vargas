29b0b1a8785a8ab5a35a7bbc971f74945f658dd3
Technexus <juanvargas14211@gmail.com>
2026-08-24 09:15:05 -0500

    Se sube el tercer y cuarto reto

--- practica-js.js
+++ practica-js.js
@@ -35,7 +35,7 @@ console.log(CalcularPromedio(4,3,4))
 */
 
 // Reto 2
-
+/*
 const cuadrado = numero => numero * numero;
 const EsMayorEdad = edad => edad >= 18;
 const NombreCompleto = (nombre, apellido) => {
@@ -44,4 +44,22 @@ const NombreCompleto = (nombre, apellido) => {
 console.log(cuadrado(3))
 console.log(EsMayorEdad(19))
 console.log(NombreCompleto("Juan", "Vargas"))
+*/
+// Reto 3
+const productos = [
+  { id: 1, nombre: 'Mouse', precio: 50000,categoria: 'Periféricos', stock: 5 },
+  { id: 2, nombre: 'Teclado', precio: 90000,categoria: 'Periféricos', stock: 0 },
+  { id: 3, nombre: 'Monitor', precio: 650000,categoria: 'Periféricos', stock: 3 },
+  { id: 4, nombre: 'Televisor', precio: 1000000,categoria: 'Pantallas', stock: 5 },
+  { id: 5, nombre: 'Nevera', precio: 2000000,categoria: 'Electrodomésticos', stock: 7 },
+  { id: 6, nombre: 'Lavadora', precio: 1500000,categoria: 'Electrodomésticos', stock: 3 },
+  { id: 7, nombre: 'Licuadora', precio: 200000,categoria: 'Electrodomésticos', stock: 2 },
+  { id: 8, nombre: 'Altavoces', precio: 21000,categoria: 'Periféricos', stock: 0 },
+  { id: 9, nombre: 'Cable HDMI', precio: 100000,categoria: 'Accesorios', stock: 0 },
+  { id: 10, nombre: 'Audifonos', precio: 30000,categoria: 'Periféricos', stock: 2 }
+];
 
+// Reto 4 
+ productos.forEach((producto, indice) => {
+  console.log(`${indice + 1}. ${producto.nombre} - ${producto.precio} - ${producto.stock}`);
+ });
