37ae68426dfa19d2156c0a1e8f0dba6464bb7d2f
Technexus <juanvargas14211@gmail.com>
2026-08-24 09:13:27 -0500

    Se sube el segundo reto

---
+++ practica-js.js
@@ -0,0 +1,47 @@
+// Reto 1
+/*
+function calcularTotal(precio, cantidad) {
+ const total = precio * cantidad;
+ return total;
+}
+const resultado = calcularTotal(50000, 3);
+console.log(resultado);
+
+function Suma(a,b){
+    const resultado = a + b 
+    return resultado
+}
+function Resta(a,b){
+    const resultado = a - b 
+    return resultado
+}
+function Multiplicar(a,b){
+    const resultado = a * b 
+    return resultado
+}
+function Dividir(a,b){
+    const resultado = a / b 
+    return resultado
+}
+function CalcularPromedio(nota1, nota2, nota3){
+    const promedio = (nota1 + nota2 + nota3) / 3
+    return promedio
+}
+console.log(Suma(2, 3))
+console.log(Resta(5, 3))
+console.log(Multiplicar(4,5))
+console.log(Dividir(10,2))
+console.log(CalcularPromedio(4,3,4))
+*/
+
+// Reto 2
+
+const cuadrado = numero => numero * numero;
+const EsMayorEdad = edad => edad >= 18;
+const NombreCompleto = (nombre, apellido) => {
+    console.log("Tu nombre completo es: " + nombre + " " + apellido);
+}
+console.log(cuadrado(3))
+console.log(EsMayorEdad(19))
+console.log(NombreCompleto("Juan", "Vargas"))
+
