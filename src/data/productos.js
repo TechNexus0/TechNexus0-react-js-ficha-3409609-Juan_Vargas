dc0c69ae84723e4d95a5e1cefa552a1c2ac12b0a
Technexus <juanvargas14211@gmail.com>
2026-08-24 10:18:46 -0500

    feat: mostrar productos con React

---
+++ src/data/productos.js
@@ -0,0 +1,12 @@
+export const productos = [
+  { id: 1, nombre: 'Mouse', precio: 50000, categoria: 'Periféricos', stock: 5 },
+  { id: 2, nombre: 'Teclado', precio: 90000, categoria: 'Periféricos', stock: 0 },
+  { id: 3, nombre: 'Monitor', precio: 650000, categoria: 'Periféricos', stock: 3 },
+  { id: 4, nombre: 'Televisor', precio: 1000000, categoria: 'Pantallas', stock: 5 },
+  { id: 5, nombre: 'Nevera', precio: 2500000, categoria: 'Electrodomésticos', stock: 7 },
+  { id: 6, nombre: 'Lavadora', precio: 1500000, categoria: 'Electrodomésticos', stock: 3 },
+  { id: 7, nombre: 'Licuadora', precio: 200000, categoria: 'Electrodomésticos', stock: 2 },
+  { id: 8, nombre: 'Altavoces', precio: 21000, categoria: 'Periféricos', stock: 0 },
+  { id: 9, nombre: 'Cable HDMI', precio: 100000, categoria: 'Accesorios', stock: 0 },
+  { id: 10, nombre: 'Audifonos', precio: 30000, categoria: 'Periféricos', stock: 2 }
+];
